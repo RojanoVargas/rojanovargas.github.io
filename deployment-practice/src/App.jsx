519aad3d4beb51c88a3681c9d30223d6c3388a9c
Eloy Vargas <rojano.vargas@gmail.com>
2024-10-29 13:52:00 +0100

    deployment practice

---
+++ deployment-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>This is simply a react deployment exercise to practice with the students</div>
+    </>
+  )
+}
+
+export default App
